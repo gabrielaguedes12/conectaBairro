a8cb7fcb614fa5970dc139f19d8dba4173d533ae
gabrielaguedes12 <gabr1.profissional@gmail.com>
2026-03-14 15:13:33 -0300

    Initial commit

---
+++ conectabairro/src/main/java/com/conectabairro/conectabairro/ConectabairroApplication.java
@@ -0,0 +1,13 @@
+package com.conectabairro.conectabairro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectabairroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectabairroApplication.class, args);
+	}
+
+}
